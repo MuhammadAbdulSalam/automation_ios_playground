deef9f1db6462218e294196b5bcf89eb11f71783
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2023-01-20 07:54:52 +0000

    initial commit

---
+++ automation_playground_ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  automation_playground_ios
+//
+//  Created by Muhammad AbdulSalam on 20/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ automation_playground_ios/automation_playground_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  automation_playground_iosApp.swift
+//  automation_playground_ios
+//
+//  Created by Muhammad AbdulSalam on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct automation_playground_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
